cca1034be19b8559e78f732c5e455a4b6b38100f
laramontana <larabrightside@gmail.com>
2017-11-01 21:25:19 +0300

    Add new painting form, Build painting_params private method

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -2,9 +2,18 @@ class PaintingsController < ApplicationController
 
   def new #new_gallery_painting GET    /galleries/:gallery_id/paintings/new
     @gallery = Gallery.find_by(params[:gallery_id])
+    @painting = @gallery.paintings.build
   end
 
-  def create # gallery_paintings_path POST   /galleries/:gallery_id/paintings
+  def create
+    @gallery = Gallery.find_by(params[:gallery_id])
+    if @gallery
+    # gallery_paintings_path POST   /galleries/:gallery_id/paintings
+  end
+
+private
+  def painting_params
+    params.require(:painting).permit(:title, :year, :style, :artist_id)
   end
 
 end
