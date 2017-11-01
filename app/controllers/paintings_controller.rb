a63f933342e71dbcf39f7a6ef610cd8b996e7f28
laramontana <larabrightside@gmail.com>
2017-11-01 22:46:53 +0300

    Complete new gallery painting form, Add messages partial to app layout

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,14 +1,23 @@
 class PaintingsController < ApplicationController
 
   def new #new_gallery_painting GET    /galleries/:gallery_id/paintings/new
-    @gallery = Gallery.find_by(params[:gallery_id])
-    @painting = @gallery.paintings.build
+    @gallery = Gallery.find_by(id: params[:gallery_id])
+    if @gallery
+      @painting = @gallery.paintings.build
+    else
+      flash[:error] = "Gallery is not found"
+      redirect_to galleries_path
+    end
   end
 
-  def create
-    @gallery = Gallery.find_by(params[:gallery_id])
-    if @gallery
-    # gallery_paintings_path POST   /galleries/:gallery_id/paintings
+  def create # gallery_paintings_path POST   /galleries/:gallery_id/paintings
+    @gallery = Gallery.find_by(id: params[:gallery_id])
+      @painting = @gallery.paintings.build(painting_params)
+      if @painting.save
+        redirect_to gallery_path(@gallery), :flash => { :notice => "Painting was successfully added!" }
+      else
+        render :new
+      end
   end
 
 private
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,10 @@
 class Painting < ApplicationRecord
   belongs_to :artist, class_name: 'User'
 	belongs_to :gallery
+
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :year, presence: true
+  validates :style, presence: true
+
 end
