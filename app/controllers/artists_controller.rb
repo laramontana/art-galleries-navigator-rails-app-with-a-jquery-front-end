e817fe0dd2b7f8a1f814904c9da77ccac60eabdd
laramontana <larabrightside@gmail.com>
2017-11-02 12:01:48 +0300

    Update artists#show view, Add artists#update action, Add artist strong params

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,4 +13,15 @@ class ArtistsController < ApplicationController
     @artist = User.find(params[:id])
   end
 
+  def update
+    @artist = User.find(params[:id])
+    @artist.update(artist_params)
+    redirect_to artist_path(@artist), :flash => { :success => "Your bio was successfully updated!" }
+  end
+
+  private
+    def artist_params
+      params.require(:user).permit(:bio)
+    end
+
 end
