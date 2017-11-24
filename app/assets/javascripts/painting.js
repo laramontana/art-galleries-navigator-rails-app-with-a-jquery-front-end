febfc914295161bb3c302ac485b4690ed4c125fc
laramontana <larabrightside@gmail.com>
2017-11-24 20:51:04 +0300

    Refactor paintings#index rendered using jQuery and AMS

---
+++ app/assets/javascripts/painting.js
@@ -0,0 +1,21 @@
+$(document).on("turbolinks:load", function(){
+  attachListeners();
+});
+
+  function attachListeners(){
+    console.log("listeners atthached")
+    $(".js-artist-paintings").on("click", showPaintings)
+  };
+
+  function showPaintings(e) {
+    e.preventDefault();
+    var id = $(this).data("id");
+    debugger
+    $.get("/artists/" + id + "/paintings.json", function(json) {
+      var template = Handlebars.compile(document.getElementById("painting-template").innerHTML);
+      for(var i=0;i<json.length;i++) {
+        var result = template(json[i]);
+        document.getElementsByClassName("painting-container")[0].innerHTML += result;
+      };
+    });
+  };
