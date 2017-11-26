f232a22adc8a8e93d94b69901bcca15310f8818b
laramontana <larabrightside@gmail.com>
2017-11-26 23:13:43 +0300

    Fix edit bio link on artist show when rendering current artist info without refresh

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -12,23 +12,37 @@ function nextArtist(e) {
   $.get("/artists/" + nextId + ".json", function(nextArtist) {
     var template = Handlebars.compile(document.getElementById("artist-info-template").innerHTML);
     $(".artist-container")[0].innerHTML = template(nextArtist);
-
-    if (parseInt($("body").attr("data-user")) !== nextArtist.id) {
-      $(".edit-bio").remove();
-      $(".add-bio").remove();
-    }
-    if (parseInt($("body").attr("data-last-user")) === nextArtist.id) { $(".js-next").remove(); }
     attachArtistsListeners();
     attachPaintingsListeners();
-    hideArtistPaintingsLink();
+    hideArtistShowLinks(nextArtist);
   });
 };
 
- function hideArtistPaintingsLink() {
-   var id = parseInt($(".artistName").attr("data-id"))
-   $.get("/artists/" + id + "/paintings.json", function(paintings) {
-     if (paintings.toString() == [].toString()) {
-       $(".js-artist-paintings")[0].innerHTML = "No paintings yet"
-     }
-   })
+function hideArtistShowLinks(artist) {
+  hideArtistPaintingsLink()
+  hideNext(artist)
+  hideEdit(artist)
 }
+
+function hideArtistPaintingsLink() {
+  var id = parseInt($(".artistName").attr("data-id"));
+  $.get("/artists/" + id + "/paintings.json", function(paintings) {
+    if (paintings.toString() == [].toString()) {
+      $(".js-artist-paintings")[0].innerHTML = "No paintings yet";
+    };
+  });
+};
+
+function hideNext(artist) {
+ if (parseInt($("body").attr("data-last-user")) === artist.id) {
+   $(".js-next").remove();
+ };
+};
+
+function hideEdit(artist) {
+ if (parseInt($("body").attr("data-user")) !== artist.id) {
+   $(".edit-bio").remove();
+ } else {
+   $(".artistBio").append(`<p><a class="edit-bio" href="/artists/${artist.id}/edit">Edit Bio</a>`)
+ }
+};
