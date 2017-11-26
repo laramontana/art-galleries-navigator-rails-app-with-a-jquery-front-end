63235c044b70aa3f8018232fdad894b7a3948c3f
laramontana <larabrightside@gmail.com>
2017-11-26 21:32:29 +0300

    Refactor showGalleryPaintings() and template

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -11,14 +11,28 @@ function showGalleryPaintings(e) {
   e.preventDefault();
   var id = $(this).data("id");
   $.get("/galleries/" + id + ".json", function(paintings) {
-
     var template = Handlebars.compile(document.getElementById("now-at-gallery-template").innerHTML);
-    for(var i=0;i<paintings.length;i++) {
-      var result = template(paintings[i]);
-      $(".now-at-gallery-ul")[0].innerHTML += result;
-    };
+    var result = template(paintings);
+    $(".now-at-gallery-ul")[0].innerHTML = result;
   });
 };
+
+function showPaintings(e) {
+  e.preventDefault();
+  var id = $(this).data("id");
+  $.get("/artists/" + id + "/paintings.json", function(paintings) {
+    var template = Handlebars.compile(document.getElementById("painting-template").innerHTML);
+    var result = template(paintings);
+    $(".painting-container")[0].innerHTML = result;
+  });
+};
+
+
+
+
+
+
+
 
 function showNewGallery(e) {
   e.preventDefault();
