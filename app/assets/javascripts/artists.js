9fbb9a86ee0bfd9bf5434d7fd365b521a4ac7589
laramontana <larabrightside@gmail.com>
2017-11-27 22:23:10 +0300

    Add Artist js model object, Refactor nextArtist() with Artist model object

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -2,6 +2,17 @@ $(document).on("turbolinks:load", function(){
   attachArtistsListeners();
 });
 
+function Artist(attributes) {
+  this.id = attributes.id;
+  this.name = attributes.name;
+  this.bio = attributes.bio;
+};
+
+Artist.prototype.getArtistInfoHTML = function() {
+  var template = Handlebars.compile($("#artist-info-template").html());
+  $(".artist-container").html(template(this));
+};
+
 function attachArtistsListeners(){
   $(".js-next").on("click", nextArtist);
 };
@@ -10,8 +21,8 @@ function nextArtist(e) {
   e.preventDefault();
   var nextId = parseInt($(".js-next").attr("data-id")) + 1;
   $.get("/artists/" + nextId + ".json", function(nextArtist) {
-    var template = Handlebars.compile($("#artist-info-template").html());
-    $(".artist-container").html(template(nextArtist));
+    var artist = new Artist(nextArtist)
+    artist.getArtistInfoHTML();
     attachArtistsListeners();
     attachPaintingsListeners();
     hideArtistShowLinks(nextArtist);
@@ -19,30 +30,26 @@ function nextArtist(e) {
 };
 
 function hideArtistShowLinks(artist) {
-  hideArtistPaintingsLink()
-  hideNext(artist)
-  hideEdit(artist)
-}
+  hideArtistPaintingsLink();
+  hideNext(artist);
+  hideEdit(artist);
+};
 
 function hideArtistPaintingsLink() {
   var artistId = parseInt($(".artistName").attr("data-id"));
   $.get("/artists/" + artistId + "/paintings.json", function(paintings) {
-    if (paintings.toString() == [].toString()) {
-      $(".js-artist-paintings").html("No paintings yet");
-    };
+    if (paintings.toString() == [].toString()) { $(".js-artist-paintings").html("No paintings yet") };
   });
 };
 
 function hideNext(artist) {
- if (parseInt($("body").attr("data-last-user")) === artist.id) {
-   $(".js-next").remove();
- };
+ if (parseInt($("body").attr("data-last-user")) === artist.id) { $(".js-next").remove() };
 };
 
 function hideEdit(artist) {
  if (parseInt($("body").attr("data-user")) !== artist.id) {
    $(".edit-bio").remove();
  } else {
-   $(".artistBio").append(`<p><a class="edit-bio" href="/artists/${artist.id}/edit">Edit Bio</a>`)
- }
+   $(".artistBio").append(`<p><a class="edit-bio" href="/artists/${artist.id}/edit">Edit Bio</a>`);
+ };
 };
